cbc0ea97117119ebb86ac59ed480795f8320d842
Jérémy Grégoire <46073105+jeyofdev@users.noreply.github.com>
2026-02-17 14:49:19 +0100

    Feat - shadcn/ui and init dark mode  (#1)
    * config: init shadcn
    
    * feat: init chadcn and dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme/theme-provider";
+import { ToggleTheme } from "@/components/ui/buttons/toggle-theme";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -23,8 +25,13 @@ const RootLayout = ({
 	children: React.ReactNode;
 }>) => {
 	return (
-		<html lang="en">
-			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+		<html lang="en" suppressHydrationWarning>
+			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+				<ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+					<ToggleTheme />
+					{children}
+				</ThemeProvider>
+			</body>
 		</html>
 	);
 };
